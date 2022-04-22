cddc074e31f5a06228b747d11fdbd34db06d8b7f
Chancesus <chancekgsmith@gmail.com>
2022-04-22 14:45:24 -0700

    Car can now turn

--- 3DTruckProject/3DTruckA3/Assets/Scripts/PlayerController.cs
+++ 3DTruckProject/3DTruckA3/Assets/Scripts/PlayerController.cs
@@ -5,16 +5,21 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 15f;
+    [SerializeField] private float turnSpeed = 10f;
 
-    // Start is called before the first frame update
+    
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+   
     void Update()
     {
-        transform.Translate(Vector3.forward * Time.deltaTime * moveSpeed);
+        var horizontalRotation = Input.GetAxis("Horizontal");
+        var changeSpeed = Input.GetAxis("Vertical");
+        
+        transform.Translate(Vector3.forward * Time.deltaTime * moveSpeed * changeSpeed);
+        transform.Rotate(Vector3.up,  Time.deltaTime * turnSpeed * horizontalRotation);
     }
 }
